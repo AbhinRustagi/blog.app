a03431b48df0ddbf855473ef4bd7a4c43ee19a90
Abhin Rustagi <rustagi.abhin@gmail.com>
2020-08-18 22:53:22 +0530

    Phase 1 Complete; No Local Storage Functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -12,22 +13,73 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
+const posts = [];
+
+app.get("/", function(req, res) {
+
+  res.render("home", {
+    homeContent: homeStartingContent,
+    postsDict: posts
+  });
+
+});
+
+app.get("/posts/:title", function(req, res) {
+  const requestedTitle = req.params.title;
+  var status = false;
+  posts.forEach(function(element) {
+    if(lodash.kebabCase(lodash.lowerCase(element.title)) === lodash.kebabCase(lodash.lowerCase(requestedTitle))){
+      status = true;
+      res.render("post", {
+        heading: element.title,
+        body: element.body
+      });
+    }
+  });
+  if (status === false) {
+    res.render("post", {
+      heading: "Error 404",
+      body: "Article not found"
+    });
+  }
+});
+
+app.get("/about", function(req, res) {
+
+  res.render("about", {
+    aboutDefaultContent: aboutContent
+  });
+});
+
+app.get("/contact", function(req, res) {
+
+  res.render("contact", {
+    contactDefaultContent: contactContent
+  });
+});
+
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
+
+app.post("/", function(req, res) {
+  const post = {
+    title: req.body.blogTitle,
+    body: req.body.blogBody
+  };
+
+  posts.push(post);
+  res.redirect("/");
+});
 
 
 
 
-
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(8888, function() {
+  console.log("Server started on port 8888");
 });
